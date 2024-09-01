374e65859c9be4154007fe8da0249eff23369977
Herry <szx21023@gmail.com>
2024-09-02 07:24:56 +0800

    add line.service.py

--- fastapi/line/__init__.py
+++ fastapi/line/__init__.py
@@ -1,17 +1,15 @@
 from linebot import LineBotApi, WebhookHandler
+from linebot.models import MessageEvent, TextMessage
 
 def init_app(app):
     from .controller import router
+    from .service import LineService
 
     app.include_router(router)
 
     app.state.line_bot_api = LineBotApi(app.state.config.get('LINE_CHANNEL_ACCESS_TOKEN'))
     app.state.line_handler = WebhookHandler(app.state.config.get('LINE_CHANNEL_SECRET'))
 
-    from linebot.models import MessageEvent, TextMessage
     @app.state.line_handler.add(MessageEvent, message=TextMessage)
     def handle_message(event: MessageEvent):
-        app.state.line_bot_api.reply_message(
-            event.reply_token,
-            TextMessage(text=event.message.text)
-        )
+        LineService.handle_message(event)
