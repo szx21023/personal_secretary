a65277574445c28bf8391c98e011b7a70cf34d0c
Herry <szx21023@gmail.com>
2024-09-02 08:28:00 +0800

    move position of asyncio.create_task

--- fastapi/line/__init__.py
+++ fastapi/line/__init__.py
@@ -1,3 +1,5 @@
+import asyncio
+
 from linebot import LineBotApi, WebhookHandler
 from linebot.models import MessageEvent, TextMessage
 
@@ -12,4 +14,4 @@ def init_app(app):
 
     @app.state.line_handler.add(MessageEvent, message=TextMessage)
     def handle_message(event: MessageEvent):
-        LineService.handle_message(event)
+        asyncio.create_task(LineService.handle_message(event))
--- fastapi/line/service.py
+++ fastapi/line/service.py
@@ -1,20 +1,11 @@
-import asyncio
 from linebot.models import MessageEvent, TextMessage
 
 from main import app
 
 class LineService:
     @staticmethod
-    def handle_message(event):
-        result = asyncio.create_task(LineService.async_function())
-        return result
-        # app.state.line_bot_api.reply_message(
-        #     event.reply_token,
-        #     TextMessage(text=event.message.text)
-        # )
-
-
-    async def async_function():
-        print(123)
-        await asyncio.sleep(1)
-        return "Hello, World!"
+    async def handle_message(event):
+        app.state.line_bot_api.reply_message(
+            event.reply_token,
+            TextMessage(text=event.message.text)
+        )
