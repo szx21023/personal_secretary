ff830ccf59ba4ad5a157380b5c3708f6edc258c9
szx21023 <szx21023@gmail.com>
2025-04-22 10:05:12 +0800

    feat: remove useless code

--- fastapi/utils.py
+++ fastapi/utils.py
@@ -27,13 +27,6 @@ def return_response(data, page_no=1, page_size=20, code=200, message=None):
 
     return ret
 
-def update_dict_with_cast(curr_conf: dict, new_conf: dict):
-    for key in curr_conf.keys():
-        if key in new_conf:
-            key_type = type(curr_conf[key])
-            cast_func = key_type if key_type in (str, int) else json.loads
-            curr_conf[key] = cast_func(new_conf[key])
-
 def system_tz(sys_tz='UTC'):
     return timezone(sys_tz)
 
