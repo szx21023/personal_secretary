03e6ca5f59b3ca5926c2dc42c38fa1a91b9b3f08
szx21023 <szx21023@gmail.com>
2025-04-24 10:00:31 +0800

    feat: from fastapi_basic.database.mongodb import MongoDB

--- fastapi/database.py
+++ fastapi/database.py
@@ -1,32 +1,17 @@
-from beanie import init_beanie
-from motor.motor_asyncio import AsyncIOMotorClient
+from fastapi_basic.database.mongodb import MongoDB
 
-from config import Settings
 from customer.model import Customer
 from daily_event.model import DailyEvent
 
-async def init_db():
-    db_settings = Settings().dict().get("MONGODB_SETTINGS")
-    username = db_settings.get("username")
-    password = db_settings.get("password")
-    host = db_settings.get("host")
-    port = db_settings.get("port")
-    db_name = db_settings.get("db_name")
-    ssl = db_settings.get("ssl")
-    ssl_ca_certs = db_settings.get("ssl_ca_certs")
-
-    db_settings = dict(
-        username=username,
-        password=password,
-        host=host,
-        port=port,
-        retryWrites=False
+async def init_db(app):
+    settings = app.state.config.get("MONGODB_SETTINGS")
+    db = MongoDB(
+        username=settings.get("username"),
+        password=settings.get("password"),
+        host=settings.get("host"),
+        port=settings.get("port"),
+        db_name=settings.get("db_name"),
+        ssl=settings.get("ssl"),
+        ssl_ca_certs=settings.get("ssl_ca_certs")
     )
-    if ssl:
-        db_settings["ssl"] = ssl
-
-    if ssl_ca_certs and ssl_ca_certs != "":
-        db_settings["tlsCAFile"] = ssl_ca_certs
-
-    client = AsyncIOMotorClient(**db_settings)
-    await init_beanie(database=client[db_name], document_models=[Customer, DailyEvent])
+    await db.connect(document_models=[Customer, DailyEvent])
