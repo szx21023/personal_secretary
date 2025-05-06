55824487bb357198df17c56f9ff8c4c82d01a980
szx21023 <szx21023@gmail.com>
2025-05-06 13:35:00 +0800

    fixed: class LineBot __init__ add input: logger

--- fastapi/line/__init__.py
+++ fastapi/line/__init__.py
@@ -1,8 +1,8 @@
 from fastapi_basic.ext.line import LineBot
 
 class LineBot(LineBot):
-    def __init__(self, channel_access_token: str, channel_secret: str):
-        super().__init__(channel_access_token, channel_secret)
+    def __init__(self, channel_access_token: str, channel_secret: str, logger=None):
+        super().__init__(channel_access_token, channel_secret, logger)
 
     async def handle_message(self, event):
         from .service import LineService
@@ -13,5 +13,5 @@ def init_app(app):
 
     app.include_router(router)
 
-    line_bot = LineBot(app.state.config.get('LINE_CHANNEL_ACCESS_TOKEN'), app.state.config.get('LINE_CHANNEL_SECRET'))
+    line_bot = LineBot(app.state.config.get('LINE_CHANNEL_ACCESS_TOKEN'), app.state.config.get('LINE_CHANNEL_SECRET'), app.logger)
     app.state.line_bot = line_bot
